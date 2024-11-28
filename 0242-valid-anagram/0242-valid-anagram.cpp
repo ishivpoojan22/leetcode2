a7965c2790a090b6d7aac35b052a26f9af269b1c
SHIV POOJAN PAL <121476533+shivpoojan22@users.noreply.github.com>
2024-11-28 12:51:54 +0530

    Time: 0 ms (100%), Space: 9.8 MB (40.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,12 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-       sort(s.begin(), s.end());
-       sort(t.begin(), t.end());
+        int freqTable[256] = {0};
 
-       if( s==t ){
+        for (int i = 0; i < s.size(); i++) {
+            freqTable[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            freqTable[t[i]]--;
+        }
+
+        for (int i = 0; i < 256; i++) {
+
+            if (freqTable[i] != 0) {
+                return false;
+            }
+        }
         return true;
-       }
-       return false;
     }
 };
