97a85db4bbe205571f22782f38090ebb3d1d92b7
SHIV POOJAN PAL <121476533+shivpoojan22@users.noreply.github.com>
2024-11-28 14:02:31 +0530

    Time: 0 ms (100%), Space: 8.1 MB (13.82%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -18,4 +18,24 @@ public:
         }
         return s;
     }
-};
+};    
+
+    // method 2 acoding to ascii value
+    // while(l<h){
+    //     if(((s[l]>=97 && s[l]<=122) || (s[l]>=65 && s[l]<=90) && 
+    //     ((s[h]>=97 && s[h]<=122) || (s[h]>=65 && s[h]<=90))){
+
+    //         swap(s[l],s[h]);
+    //         l++;
+    //         h--;
+    //     }
+    //     else if(!((s[l] >= 97 && s[l] <= 122) || (s[l] >= 65 && s[l] <= 90))){
+    //         l++;
+    //     }
+    //     else{
+    //         s[h]-->not aplha
+    //         h--;
+    //     }
+
+    // }
+    // return s;
